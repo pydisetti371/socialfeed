6d43d8d98b1418c495dca0638519527db41a429f
revanth371 <revanth.p@craftvedatechnology.com>
2020-10-28 08:33:53 +0530

    attached social media name

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ import { getFaceBook, getLinkedin, getTwitter } from "./redux/action";
 class App extends React.Component {
   componentDidMount() {
     let facebook = [
-      { post: "@JohnDoe", time: "03:01pm" },
-      { post: "@Chiranjeevi", time: "04:05pm" },
-      { post: "@Rajanikanth", time: "05:10pm" },
-      { post: "@Dhawan", time: "06:15pm" },
-      { post: "@Tamada media", time: "07:20pm" },
+      { post: "@JohnDoe", time: "03:01pm", name: "facebook" },
+      { post: "@Chiranjeevi", time: "04:05pm", name: "facebook" },
+      { post: "@Rajanikanth", time: "05:10pm", name: "facebook" },
+      { post: "@Dhawan", time: "06:15pm", name: "facebook" },
+      { post: "@Tamada media", time: "07:20pm", name: "facebook" },
     ];
     localStorage.setItem("faceBook", JSON.stringify(facebook));
     let localStorageData = JSON.parse(localStorage.getItem("faceBook") || "[]");
     this.props.dispatch(getFaceBook(localStorageData));
 
     let twitter = [
-      { post: "@pm modi", time: "03:05pm" },
-      { post: "@rgv", time: "04:10pm" },
-      { post: "@vknaidu", time: "05:15pm" },
-      { post: "@Cbn", time: "06:20pm" },
-      { post: "@pk", time: "07:25pm" },
+      { post: "@pm modi", time: "03:05pm", name: "twitter" },
+      { post: "@rgv", time: "04:10pm", name: "twitter" },
+      { post: "@vknaidu", time: "05:15pm", name: "twitter" },
+      { post: "@Cbn", time: "06:20pm", name: "twitter" },
+      { post: "@pk", time: "07:25pm", name: "twitter" },
     ];
     localStorage.setItem("twitter", JSON.stringify(twitter));
     let twitterData = JSON.parse(localStorage.getItem("twitter") || "[]");
     this.props.dispatch(getTwitter(twitterData));
 
     let linkedin = [
-      { post: "@revanth-pydisetti", time: "03:10pm" },
-      { post: "@billgates", time: "04:15pm" },
-      { post: "@sundar-pichay", time: "05:20pm" },
-      { post: "@mark", time: "06:25pm" },
-      { post: "@Rahul", time: "07:30pm" },
+      { post: "@revanth-pydisetti", time: "03:10pm", name: "linkedin" },
+      { post: "@billgates", time: "04:15pm", name: "linkedin" },
+      { post: "@sundar-pichay", time: "05:20pm", name: "linkedin" },
+      { post: "@mark", time: "06:25pm", name: "linkedin" },
+      { post: "@Rahul", time: "07:30pm", name: "linkedin" },
     ];
     localStorage.setItem("linkedin", JSON.stringify(linkedin));
     let linkedinData = JSON.parse(localStorage.getItem("linkedin") || "[]");
@@ -46,16 +46,31 @@ class App extends React.Component {
       <div>
         <ul>
           <li>
-            {faceBookDetails[0].post} "-"
-            {faceBookDetails[0].time}
+            {faceBookDetails && faceBookDetails[0] && faceBookDetails[0].post}{" "}
+            "-"
+            {faceBookDetails &&
+              faceBookDetails[0] &&
+              faceBookDetails[0].time}{" "}
+            "-"
+            {faceBookDetails && faceBookDetails[0] && faceBookDetails[0].name}
           </li>
           <li>
             {" "}
-            {twitterDetails[0].post} "-"{twitterDetails[0].time}
+            {twitterDetails && twitterDetails[0] && twitterDetails[0].post} "-"
+            {twitterDetails &&
+              twitterDetails[0] &&
+              twitterDetails[0].time} "-"{" "}
+            {twitterDetails && twitterDetails[0] && twitterDetails[0].name}
           </li>
           <li>
             {" "}
-            {faceBookDetails[1].post} "-"{faceBookDetails[1].time}
+            {faceBookDetails &&
+              faceBookDetails[1] &&
+              faceBookDetails[1].post}{" "}
+            "-"
+            {faceBookDetails && faceBookDetails[1] && faceBookDetails[1].time}{" "}
+            "-"{" "}
+            {faceBookDetails && faceBookDetails[1] && faceBookDetails[1].name}
           </li>
         </ul>
       </div>
